95ce2015997ee9b85aa5da4de9d013f053f7546d
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2019-07-11 12:26:26 +0200

    added bulma

--- src/client/components/BodyDispVirt.js
+++ src/client/components/BodyDispVirt.js
@@ -4,10 +4,12 @@ import phone2 from '../../img/pictureOneCard.png';
 
 export default function BodyDispVirt() {
 	return (
-		<div>
+		<div class='container'>
 			<p>TEST</p>
 			<img src={phone1} alt='logo' style={{ height: 600, width: 380 }} />
 			<img src={phone2} alt='logo' />
 		</div>
 	);
 }
+
+
