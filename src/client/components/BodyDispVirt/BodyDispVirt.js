ece087bdc9669df4e2a0a3b970b92ca9629140a8
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2019-07-30 14:17:19 +0200

    housekeeping

--- src/client/components/BodyDispVirt/BodyDispVirt.js
+++ src/client/components/BodyDispVirt/BodyDispVirt.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../../App.css';
-import Button from '../../elements/Button/Button';
+import Button from '../Button/Button';
 
 export default function BodyDispVirt() {
 	return (
