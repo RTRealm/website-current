b56109e197babadd1110d285a19279cec12006fc
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2019-07-30 13:03:18 +0200

    working on Page structure v2

--- src/client/containers/Home/Home.js
+++ src/client/containers/Home/Home.js
@@ -3,6 +3,7 @@ import React, { PureComponent } from 'react';
 // page sections
 import BodyDispVirt from '../../components/BodyDispVirt/BodyDispVirt';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 // components
 import Page from '../../components/Page/index';
@@ -11,7 +12,9 @@ class Home extends PureComponent {
 	render() {
 		return (
 			<Page>
+				<Header />
 				<BodyDispVirt />
+				<Footer />
 			</Page>
 		);
 	}
