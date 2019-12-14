bcdc344e2e16a75729420c7cadec19157e25d212
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2019-12-14 17:15:18 +0100

    mobile content - headers

--- src/client/containers/Home/FeaturesList/FeaturesList.js
+++ src/client/containers/Home/FeaturesList/FeaturesList.js
@@ -8,7 +8,7 @@ const FeaturesList = () => {
 			<Styles.FeaturesListContent>
 				<Styles.FeaturesListItem className='features-list__item--sync'>
 					<span />
-					<h3>All your cards in one</h3>
+					<h3>Software house and more...</h3>
 					<p>
 						Add unlimited debit and credit cards to your Curve App and choose which one you want to
 						pay with from the App using your Curve Card
@@ -16,7 +16,7 @@ const FeaturesList = () => {
 				</Styles.FeaturesListItem>
 				<Styles.FeaturesListItem className='features-list__item--insights'>
 					<span />
-					<h3>Get instant insights</h3>
+					<h3>Development</h3>
 					<p>
 						Curve helps you take back control of your spending by showing you rich insights
 						instantly in the App
@@ -24,7 +24,7 @@ const FeaturesList = () => {
 				</Styles.FeaturesListItem>
 				<Styles.FeaturesListItem className='features-list__item--abroad'>
 					<span />
-					<h3>Save money abroad</h3>
+					<h3>Tech Consultancy</h3>
 					<p>No currency exchange fees when spending abroad with Curve.</p>
 				</Styles.FeaturesListItem>
 			</Styles.FeaturesListContent>
