2468a7ecb65e3c81a39b860c3cc2a58ccb11aad2
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2019-12-31 13:16:09 +0100

    no images

--- src/client/containers/Home/Intro/Intro.js
+++ src/client/containers/Home/Intro/Intro.js
@@ -19,12 +19,12 @@ const intro = () => {
 	return (
 		<Styles.Intro className='columns'>
 			<Styles.IntroImage className='is-6 column'>
-				<img
+				{/* <img
 					src='https://res.cloudinary.com/dzatxn6bx/image/upload/v1536831291/website-v2/Travel/main-card-m.png'
 					srcSet='https://res.cloudinary.com/dzatxn6bx/image/upload/v1536831292/website-v2/Travel/main-card-m_2x.png 2x, https://res.cloudinary.com/dzatxn6bx/image/upload/v1536855793/website-v2/Travel/main-card.png 665w'
 					alt='Curve card'
 					className='is-hidden-tablet'
-				/>
+				/> */}
 				<img
 					src='https://res.cloudinary.com/ohcash/image/upload/v1576925844/landingpage/Intro1-large.png'
 					srcSet='https://res.cloudinary.com/ohcash/image/upload/v1576925844/landingpage/Intro1-large.png 2x'
