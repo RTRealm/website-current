231b8e3d0c6d264d8c2fa71ff770ad96a9695a44
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2020-01-02 19:21:52 +0100

    working on localisation...

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Routes from './client/containers/routes';
 import ScrollToTop from './client/components/ScrollToTop/ScrollToTop';
 import * as Styles from './App.styles';
 
+import { languageOptions } from '../src/client/languages';
+
 import { LanguageProvider } from './client/containers/Language';
 
 class App extends Component {
-	componentDidMount() {
+	async componentDidMount() {
 		window.scrollTo(0, 0);
 	}
 
