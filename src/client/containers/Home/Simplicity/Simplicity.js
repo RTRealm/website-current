284bdb304dfa54343ce50fffdb03e99be7b16212
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2020-01-09 13:59:57 +0100

    infinities and map in contact section

--- src/client/containers/Home/Simplicity/Simplicity.js
+++ src/client/containers/Home/Simplicity/Simplicity.js
@@ -89,12 +89,12 @@ class Simplicity extends Component {
 										alt='CFA'
 									/>
 								</Styles.SimplicityFeaturesItem>
-								<Styles.SimplicityFeaturesItem className='column simplicity__features-item'>
-									{/* <img
+								{/* <Styles.SimplicityFeaturesItem className='column simplicity__features-item'>
+									<img
 										src='https://res.cloudinary.com/ohcash/image/upload/v1578144498/landingpage/cfa-logo.svg'
 										alt='CFA'
-									/> */}
-								</Styles.SimplicityFeaturesItem>
+									/>
+								</Styles.SimplicityFeaturesItem> */}
 								{/* <Styles.SimplicityFeaturesItem className='column simplicity__features-item'>
 									<img
 										src='https://res.cloudinary.com/dzatxn6bx/image/upload/v1536844208/website-v2/Icons/receipt.svg'
