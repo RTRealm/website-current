a1c6043c64fbc7ee5dbb41d88465d45ccaecb335
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2020-01-13 09:47:08 +0100

    working on routes

--- src/client/containers/routes.js
+++ src/client/containers/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Loadable from 'react-loadable';
 
 import CookieBanner from '../../client/components/CookieBanner/CookieBanner';
