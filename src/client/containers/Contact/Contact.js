69577c37638497bbd76e3fbba544d8b9badbd57f
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2023-01-03 10:34:10 +0100

    clickable phone number

--- src/client/containers/Contact/Contact.js
+++ src/client/containers/Contact/Contact.js
@@ -65,7 +65,7 @@ class Contact extends Component {
 						<Styles.ContactSecurity>
 							<p>
 								{this.textBody('contactPhone')}{' '}
-									<a> +48 737 337 552 </a>
+									<a href="tel:+48737337552"> +48 737 337 552 </a>
 									{' '}{this.textBody('contactEmail')}{' '}
 								<a href="mailto:contact@rtrealm.com?subject=Let's talk!& body=Contact me asap on (phone or email):">
 									contact@rtrealm.com
